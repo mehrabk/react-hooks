ff782494cf3be26f9488aafe242ce1e788e90088
mehrabk <mehrab.kor@gmail.com>
2021-11-25 21:10:18 +0330

    termDebounce state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("javascript");
+  const [debounceTerm, setDebounceTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   // console.log("I Run With Every Render");
@@ -30,6 +31,19 @@ const Search = () => {
   //   })();
   // });
 
+  // if we not have debounce state and directly use term for dependency of useEffect and setTime and fetch
+  // then we type for exmaple javascript and then immediately redo to javascript request send to api
+  // ==> for inhance performance we use debounceTerm state and send request with useEffect that have debounceTerm dependency
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebounceTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -41,10 +55,29 @@ const Search = () => {
           srsearch: term,
         },
       });
-      setResults(data);
+      setResults(data.query.search);
     };
     fetchData();
-  }, [term]);
+  }, [debounceTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -58,6 +91,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
